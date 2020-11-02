bad5014f7d66710933f4c37d23cf3047df4c12c1
Lucas Rufino <lucaslrt@outlook.com>
2020-11-02 16:41:01 -0300

    Adicionando status para o personagem.

---
+++ Assets/Scripts/CharacterMovementVelocity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementVelocity : MonoBehaviour
+{
+    [SerializeField] private int moveSpeed = 5;
+
+    private Vector3 velocityVector;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void SetVelocity(Vector3 velocityVector)
+    {
+        this.velocityVector = velocityVector;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = velocityVector * moveSpeed;
+    }
+}
---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    [SerializeField] private string charName;
+    [SerializeField] private int life;
+    [SerializeField] private int strenght;
+
+    public int Life { get => life; set => life = value; }
+    public int Strenght { get => strenght; set => strenght = value; }
+    public string CharName { get => charName; set => charName = value; }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
